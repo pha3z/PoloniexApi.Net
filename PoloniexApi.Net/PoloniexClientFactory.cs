a176dd3443df37f9479fef2f15959beeded0f367
Sean O'Brien <seanmobrien@hotmail.com>
2017-07-16 02:07:36 -0500

    Implemented IPoloniexClientFactory

--- PoloniexApi.Net/PoloniexClientFactory.cs
+++ PoloniexApi.Net/PoloniexClientFactory.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 namespace Jojatekok.PoloniexAPI
 {
     public class PoloniexClientFactory
+        : IPoloniexClientFactory
     {
+        public virtual IPoloniexClient CreateAnonymousClient()
+        {
+            return new PoloniexClient();
+        }
+
+        public virtual IPoloniexClient CreateClient(string publicKey, string privateApiKey)
+        {
+            return new PoloniexClient(publicKey, privateApiKey);
+        }
     }
 }
