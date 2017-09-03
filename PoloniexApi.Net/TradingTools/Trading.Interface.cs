bf9c3ef138f3339728f094b65a37a9bb68917a5c
Sean O'Brien <seanmobrien@hotmail.com>
2017-09-03 03:03:59 -0500

    Added support for additional margin-related trade api methods

--- PoloniexApi.Net/TradingTools/Trading.Interface.cs
+++ PoloniexApi.Net/TradingTools/Trading.Interface.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 
 namespace Jojatekok.PoloniexAPI
 {
+    /// <summary>
+    /// The <see cref="ITrading"/> interface defines methods used to interact with Poloniex&apos;s trading service.
+    /// </summary>
     public interface ITrading
     {
         /// <summary>Fetches the current open orders in your account, ordered by most recent first.</summary>
@@ -15,6 +18,27 @@ namespace Jojatekok.PoloniexAPI
         /// <param name="currencyPair">The currency pair, which consists of the currency being traded on the market, and the base's code.</param>
         Task<IList<ITrade>> GetTradesAsync(CurrencyPair currencyPair);
 
+        /// <summary>Fetches information about your current margin position.</summary>
+        /// <param name="currencyPair">The currency pair, which consists of the currency being traded on the market, and the base's code.</param>
+        /// <returns>A <see cref="IPosition"/> instance that describes the active position, if any.</returns>
+        Task<IPosition> GetMarginPositionAsync(CurrencyPair currencyPair);
+
+
+        /// <summary>Fetches information about your current margin position.</summary>
+        /// <returns>A <see cref="IMarginAccountSummary"/> instance that describes the active position, if any.</returns>
+        Task<IMarginAccountSummary> GetMarginAccountSummaryAsync();
+
+
+        /// <summary>Fetches information about your current margin position.</summary>
+        /// <param name="currencyPair">The currency pair, which consists of the currency being traded on the market, and the base's code.</param>
+        /// <returns>A <see cref="IPosition"/> instance that describes the active position, if any.</returns>
+        Task<IList<ITrade>> CloseMarginPositionAsync(CurrencyPair currencyPair);
+
+
+        /// <summary>Fetches information about your current margin position.</summary>
+        /// <returns>A <see cref="IPosition"/> instance that describes the active position, if any.</returns>
+        Task<IDictionary<CurrencyPair, IPosition>> GetAllMarginPositionsAsync();
+        
         /// <summary>Fetches the trades made in your account in a given time period, ordered by most recent first.</summary>
         /// <param name="currencyPair">The currency pair, which consists of the currency being traded on the market, and the base's code.</param>
         /// <param name="startTime">The time to start fetching data from.</param>
@@ -27,6 +51,13 @@ namespace Jojatekok.PoloniexAPI
         /// <param name="pricePerCoin">The price to trade your coins at, compared to the base currency.</param>
         /// <param name="amountQuote">The amount of quote you want to trade.</param>
         Task<string> PostOrderAsync(CurrencyPair currencyPair, OrderType type, double pricePerCoin, double amountQuote);
+
+        /// <summary>Cancels an order and places a new one of the same type in a single atomic transaction, meaning either both operations will succeed or both will fail.</summary>
+        /// <param name="orderId">The ID of the order to cancel.</param>
+        /// <param name="newRate">The new rate for the order</param>
+        /// <param name="amount">Optional change in amount.</param>
+        Task<string> MoveOrderAsync(ulong orderId, double newRate, double? amount = default(double?));
+
 
         /// <summary>
         ///     <para>Cancels an open order identified by the order ID.</para>
--- PoloniexApi.Net/TradingTools/Trading.cs
+++ PoloniexApi.Net/TradingTools/Trading.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Jojatekok.PoloniexAPI.Exceptions;
 
 namespace Jojatekok.PoloniexAPI.TradingTools
 {
@@ -47,10 +48,8 @@ namespace Jojatekok.PoloniexAPI.TradingTools
             };
 
             var data = PostData<JObject>(type.ToStringNormalized(), postData);
-            if(data.Value<string>("error") != null)
-                return data.Value<string>("error");
 
-            return data.Value<string>("orderNumber");
+            return ThrowForFailure(data).Value<string>("orderNumber");
         }
 
         private string PostMarginOrder(CurrencyPair currencyPair, OrderType type, double pricePerCoin, double amountQuote, double lendingRate)
@@ -63,10 +62,8 @@ namespace Jojatekok.PoloniexAPI.TradingTools
             };
 
             var data = PostData<JObject>(type.ToStringNormalized(), postData);
-            if (data.Value<string>("error") != null)
-                return data.Value<string>("error");
 
-            return data.Value<string>("orderNumber");
+            return ThrowForFailure(data).Value<string>("orderNumber");
         }
 
         private bool DeleteOrder(CurrencyPair currencyPair, ulong orderId)
@@ -77,7 +74,97 @@ namespace Jojatekok.PoloniexAPI.TradingTools
             };
 
             var data = PostData<JObject>("cancelOrder", postData);
-            return data.Value<byte>("success") == 1;
+            return ThrowForFailure(data).Value<byte>("success") == 1;
+        }
+
+
+        private IPosition GetMarginPosition(CurrencyPair currencyPair)
+        {
+            var postData = new Dictionary<string, object> {
+                { "currencyPair", currencyPair }
+            };
+
+            var data = PostData<Position>("getMarginPosition", postData);
+            return data != null && data.Type != "none"
+                ? data
+                : default(IPosition);
+        }
+
+        private IDictionary<CurrencyPair, IPosition> GetAllMarginPositions()
+        {
+            var postData = new Dictionary<string, object> {
+                { "currencyPair", "all" }
+            };
+            var data = PostData<IDictionary<string, Position>>("getMarginPosition", postData);
+            return data
+                    .Where(x => x.Value.Type != "none")
+                    .ToDictionary(
+                        x => CurrencyPair.Parse(x.Key), 
+                        x => (IPosition) x.Value);
+        }
+
+
+        private IList<ITrade> CloseMarginPosition(CurrencyPair currencyPair)
+        {
+            var postData = new Dictionary<string, object> {
+                { "currencyPair", currencyPair }
+            };
+
+            var data = PostData<JObject>("closeMarginPosition", postData);
+
+            var trades = ThrowForFailure(data).Value<Dictionary<string, IList<Trade>>>("resultingTrades");
+            return trades != null && trades.Any()
+                ? trades.SelectMany(x => x.Value).ToList<ITrade>()
+                : new List<ITrade>();
+        }
+
+        string MoveOrder(ulong orderId, double newRate, double? amount = null, TradeFlags flags = TradeFlags.None)
+        {
+            var postData = new Dictionary<string, object> {
+                { "orderNumber", orderId },
+                { "rate", newRate }
+            };
+            if(amount.HasValue) {  postData["amount"] = amount.Value; }
+            postData = AddTradeFlags(postData, flags);
+
+            var data = PostData<JObject>("moveOrder", postData);
+            
+            return ThrowForFailure(data).Value<string>("orderNumber");
+        }
+
+
+        Dictionary<string, object> AddTradeFlags(Dictionary<string, object> target, TradeFlags flags)
+        {
+            if (flags.HasFlag(TradeFlags.ImmediateOrCancel)) { target["immediateOrCancel"] = 1; }
+            if (flags.HasFlag(TradeFlags.PostOnly)) { target["postOnly"] = 1; }
+            return target;
+        }
+
+        IMarginAccountSummary GetMarginAccountSummary()
+        {
+            var postData = new Dictionary<string, object> {
+                };
+            var data = PostData<MarginAccountSummary>("returnMarginAccountSummary", postData);
+            return data;
+        }
+
+        public Task<IList<ITrade>> CloseMarginPositionAsync(CurrencyPair currencyPair)
+        {
+            return Task.Factory.StartNew(() => CloseMarginPosition(currencyPair));
+        }
+
+        public Task<IDictionary<CurrencyPair, IPosition>> GetAllMarginPositionsAsync()
+        {
+            return Task.Factory.StartNew(GetAllMarginPositions);
+        }
+        public Task<IPosition> GetMarginPositionAsync(CurrencyPair currencyPair)
+        {
+            return Task.Factory.StartNew(() => GetMarginPosition(currencyPair));
+        }
+
+        public Task<IMarginAccountSummary> GetMarginAccountSummaryAsync()
+        {
+            return Task.Factory.StartNew(GetMarginAccountSummary);
         }
 
         public Task<IList<IOrder>> GetOpenOrdersAsync(CurrencyPair currencyPair)
@@ -100,6 +187,11 @@ namespace Jojatekok.PoloniexAPI.TradingTools
             return Task.Factory.StartNew(() => PostOrder(currencyPair, type, pricePerCoin, amountQuote));
         }
 
+        public Task<string> MoveOrderAsync(ulong orderId, double newRate, double? amount = null)
+        {
+            return Task.Factory.StartNew(() => MoveOrder(orderId, newRate, amount));
+        }
+
         public Task<string> PostMarginOrderAsync(CurrencyPair currencyPair, OrderType type, double pricePerCoin, double amountQuote, double lendingRate)
         {
             return Task.Factory.StartNew(() => PostMarginOrder(currencyPair, type, pricePerCoin, amountQuote, lendingRate));
@@ -111,9 +203,19 @@ namespace Jojatekok.PoloniexAPI.TradingTools
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private JObject ThrowForFailure(JObject data, bool checkSuccess = true)
+        {
+            if (data.Value<string>("error") != null) { throw new TradeOperationFailureException(data); }
+            if (checkSuccess && data.Value<byte>("success") != 1) { throw new TradeOperationFailureException(data); }
+            return data;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private T PostData<T>(string command, Dictionary<string, object> postData)
         {
             return ApiWebClient.PostData<T>(command, postData);
         }
+
+     
     }
 }
